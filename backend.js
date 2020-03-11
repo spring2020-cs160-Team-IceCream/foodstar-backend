41c730bdcfed68ee08d05df7d179f8d51735f77f
nanoparticle <nanoparticle@users.noreply.github.com>
2020-03-11 15:17:10 -0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/api', (req, res) => res.send('Hello World!'))
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('foodstar', 'dbaccess', '12345', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+app.get('/api', (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send('DB connected!')
+  } catch (error) {
+    res.send('DB connection failed!')
+  }
+  //res.send('Hello World!')
+})
 
 app.listen(port, () => console.log("Example app listening on port ${port}!"))
