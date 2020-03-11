b9cd11598a359766a4f540cc76e0e20681d157b4
nanoparticle <nanoparticle@users.noreply.github.com>
2020-03-11 14:59:39 -0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = 'localhost';
-const port = 3000;
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World Test 4!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
