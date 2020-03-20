27d5827d5a8627d237f838cb576c4d3fee939117
nanoparticle <nanoparticle@users.noreply.github.com>
2020-03-19 21:12:58 -0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -14,7 +14,10 @@ sequelize.define('Authentication', {
   password: {type: DataTypes.STRING},
   startsalt: {type: DataTypes.STRING},
   endsalt: {type: DataTypes.STRING},
-  user_id_fk: {type: DataTypes.INTEGER}
+  user_id_fk: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  }
 }, {
   // Other model options go here
   tableName: 'authentication'
