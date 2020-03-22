fb3b50e9bd5a56511cac17be62f3f1f6bf7d6eaf
Jason Kmec <mcnanoparticle@gmail.com>
2020-03-21 20:59:45 -0700

    Test models outside of main file

--- backend.js
+++ backend.js
@@ -2,31 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const {Sequelize, DataTypes} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize('foodstar', 'dbaccess', '12345', {
   host: 'localhost',
   dialect: 'mysql'
 });
 
-sequelize.define('Authentication', {
-  // Model attributes are defined here
-  username: {type: DataTypes.STRING},
-  password: {type: DataTypes.STRING},
-  startsalt: {type: DataTypes.STRING},
-  endsalt: {type: DataTypes.STRING},
-  user_id_fk: {
-    type: DataTypes.INTEGER,
-    primaryKey: true
-  }
-}, {
-  // Other model options go here
-  tableName: 'authentication',
-  timestamps: false
-});
+const Authentication = require('./models/authentication')(sequelize, Sequelize)
 
 
 app.get('/api', async (req, res) => {
-  const users = await sequelize.models.Authentication.findAll();
+  const users = await Authentication.findAll();
   res.send(users)
 })
 
---
+++ models/authentication.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('Authentication', {
+        // Model attributes are defined here
+        username: { type: DataTypes.STRING },
+        password: { type: DataTypes.STRING },
+        startsalt: { type: DataTypes.STRING },
+        endsalt: { type: DataTypes.STRING },
+        user_id_fk: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        }
+    }, {
+        // Other model options go here
+        tableName: 'authentication',
+        timestamps: false
+    });
+}
+
